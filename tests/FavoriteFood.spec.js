b9e383f1e5aef0211f9481a11451aeb29ed28680
Chen Yang <yangc5@mail.uc.edu>
2016-03-23 18:54:34 +0000

    done

--- tests/FavoriteFood.spec.js
+++ tests/FavoriteFood.spec.js
@@ -1,11 +1,28 @@
 describe('Favorite Food Filter', function () {
-	var $controller;
+	var $controller, favoriteFood;
 
 	beforeEach(module('app'));
 
-	beforeEach(inject(function ($injector) {
-		$filter = $injector.get('$filter');
-	}));
+  beforeEach(inject(function ($filter) {
+      favoriteFood = $filter('favoriteFood');
+    }));
 
-	
+ it('return only items with favorite food', function () {
+     var mockedList = [{
+			name: 'Test',
+			favoriteFood: 'Bread'
+		}, {
+			name: 'Test 2',
+			favoriteFood: 'Apples'
+		}, {
+			name: 'Test 3',
+			favoriteFood: 'Carrots'
+		}];
+
+		var results = favoriteFood(mockedList, 'Apples');
+
+		expect(results.length).toBe(1);
+		expect(results[0].name).toBe('Test 2');
+  });
+
 });
--- tests/removeAllVowels.spec.js
+++ tests/removeAllVowels.spec.js
@@ -1,10 +1,14 @@
 describe('removeAllVowels Filter', function () {
-	var $controller;
+	var $controller, removeAllVowels;
 
 	beforeEach(module('app'));
 
-	beforeEach(inject(function ($injector) {
-		$filter = $injector.get('$filter');
+	beforeEach(inject(function ($filter) {
+		removeAllVowels = $filter('removeAllVowels');
 	}));
+
+  it('should remove all vowels', function(){
+     expect(removeAllVowels('aeipq')).toBe('pq');
+  });
 
 });
